408645051acb1cc282e0dbb21270ea2a0f47c7fd
Aditya Agrawal <aditya999123@gmail.com>
2022-10-07 13:11:13 +0530

    Create fixture.sql

---
+++ btree_gin/fixture.sql
@@ -0,0 +1,33 @@
+create table users(
+   id serial primary key
+);
+
+insert into users select * from generate_series(1, 200000);
+
+create table orgs(
+   id serial primary key
+);
+
+insert into orgs select * from generate_series(1,1000);
+
+create table expenses(
+   id serial primary key,
+   org_id integer references orgs(id) not null,
+   user_ids integer[] not null
+);
+
+insert into expenses 
+   select 
+       id, 
+       1+id%999 org_id, 
+       gi.user_ids
+   from 
+      generate_series(1,5000000) id
+   join lateral (
+      select 
+          i%5000000 pid, 
+          array_agg(round(random() * 200000)) user_ids
+      from generate_series(1, 5.5*5000000) i
+      group by i%5000000
+   ) gi on pid=id
+;
